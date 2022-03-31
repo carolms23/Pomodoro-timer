a37400499e31eaf351fb8820e72420dfc20c4c3b
carolms23 <carolinems360@gmail.com>
2022-03-31 20:30:17 -0300

    alteraçoes javascript

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const startStop = document.getElementById('start-stop');
+const test = document.getElementsByClassName("personal")
+let minutes = document.getElementById('minutes'),
+    seconds = document.getElementById('seconds')
+console.log(test)
+startStop.addEventListener('click',()=>{
+   startStop.innerHTML ="PAUSAR"
+    return startStop.innerHTML
+    startProgress()
+})
+
+
+
+    
+function startProgress(){
+    
+}
