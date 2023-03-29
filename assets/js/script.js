cba0201162d57feb83687cec12735bdb3f69d345
carolms23 <carolinems360@gmail.com>
2023-03-28 22:12:25 -0300

    feature add function to reset timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,44 @@
-const startStop = document.getElementById('start-stop');
-const test = document.getElementsByClassName("personal")
-let minutes = document.getElementById('minutes'),
-    seconds = document.getElementById('seconds')
-console.log(test)
-startStop.addEventListener('click',()=>{
-   startStop.innerHTML ="PAUSAR"
-    return startStop.innerHTML
-    startProgress()
-})
+const startStop = document.getElementById("start-stop");
+
+const minutesHTML = document.getElementById("minutes"),
+  secondsHTML = document.getElementById("seconds");
+
+let interval;
+
+startStop.addEventListener("click", () => {
+  startStop.innerHTML = "PAUSAR";
+  changingTime();
+  return startStop.innerHTML;
+});
+
+const showTimeScreen = (minutes, seconds) => {
+  minutesHTML.innerHTML = minutes;
+  secondsHTML.innerHTML = seconds;
+};
+
+const converterTime = (timer) => {
+  return { seconds: timer % 60, minutes: Math.floor(timer / 60) };
+  
+};
+
+function changingTime() {
+  interval = setInterval(subOneSecond, 1000);
 
 
-
-    
-function startProgress(){
-    
 }
+const subOneSecond = () => {
+  timer--;
+  const result = converterTime(timer);
+  showTimeScreen(result.minutes, result.seconds);
+  if(result.minutes < 0 && result.seconds < 0){
+    (clearInterval(interval));
+    showTimeScreen("00","00");
+  }
+}
+let timer = 01 * 60;
+
+
+
+
+
+
